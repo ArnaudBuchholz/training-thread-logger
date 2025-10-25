9933ed0f0717543e0467a22e536fbf4b92c19ab1
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-10-25 00:05:04 -0400

    refactor: removed db, use compression on top of gzip

--- threads/logger.ts
+++ threads/logger.ts
@@ -1,50 +1,16 @@
 import { BroadcastChannel, threadId, isMainThread } from 'worker_threads';
 import type { InternalLogAttributes, LogAttributes } from '../logger.ts';
 import path from 'node:path';
-import { writeFileSync, createWriteStream } from 'fs';
-import { DatabaseSync } from 'node:sqlite';
+import { writeFileSync, createWriteStream } from 'node:fs';
 import zlib from 'zlib';
 
 const LOG_FILE_NAME = `app-${new Date().toISOString().slice(0, 19).replaceAll(/[-:]/g, '').replace('T', '-')}.log`;
-console.log(LOG_FILE_NAME);
 const txtLogFilePath = path.join(process.cwd(), LOG_FILE_NAME + '.jsonl');
 
-const db = new DatabaseSync(path.join(process.cwd(), LOG_FILE_NAME + '.db'));
-db.exec(`
-CREATE TABLE logs (
-  uid INT,
-  level TEXT,
-  timestamp DATE,
-  processId INT,
-  threadId INT,
-  isMainThread BOOL,
-  message TEXT
-)
-`);
-const dbLog = db.prepare(`
-INSERT INTO logs(
-  uid,
-  level,
-  timestamp,
-  processId,
-  threadId,
-  isMainThread,
-  message
-) VALUES (
-  :uid,
-  :level,
-  :timestamp,
-  :processId,
-  :threadId,
-  :isMainThread,
-  :message
-)
-`);
-
 const fileStream = createWriteStream(path.join(process.cwd(), LOG_FILE_NAME + '.gz'));
-const gzipStream = zlib.createGzip();
+const gzipStream = zlib.createGzip({ flush: zlib.constants.Z_FULL_FLUSH });
 gzipStream.pipe(fileStream);
-const gzBuffer: object[] = [];
+const gzBuffer: (string | [string, object])[] = [];
 let gzFlushTimeout: ReturnType<typeof setTimeout> | undefined;
 const GZ_MAX_BUFFER_SIZE = 50;
 const GZ_FLUSH_INTERVAL_MS = 200;
@@ -55,30 +21,20 @@ const gzFlushBuffer = () => {
   }
 
   const dataToWrite = gzBuffer.map(log => JSON.stringify(log)).join('\n') + '\n';
-  gzipStream.write(dataToWrite);
+  console.log('writing to stream', gzipStream.write(dataToWrite));
 
   gzBuffer.length = 0;
   clearTimeout(gzFlushTimeout);
   gzFlushTimeout = undefined;
 };
 
-
-let uid = 0
+const reduceNumber = (value: number) => Number(value).toString(36);
 
 const log = (attributes: InternalLogAttributes & LogAttributes) => {
-  const { level, timestamp, processId = 0, threadId = 0, message }  = attributes;
-  const icon = {
-    debug: '🐞',
-    info: '🛈',
-    warn: '⚠️',
-    error: '❌',
-    fatal: '💣'
-  }[level];
-  console.log(++uid, icon, timestamp, processId, threadId, message);
-  writeFileSync(txtLogFilePath, JSON.stringify({ uid, ...attributes }) + '\n', { encoding: 'utf8', flag: 'a' })
-  const { error, ...otherAttributes } = attributes;
-  dbLog.run({ uid, ...otherAttributes, isMainThread: attributes.isMainThread ? 'true' : 'false' });
-  gzBuffer.push(attributes);
+  writeFileSync(txtLogFilePath, JSON.stringify(attributes) + '\n', { encoding: 'utf8', flag: 'a' })
+  const { level, timestamp, processId, threadId, isMainThread, source, message, data } = attributes;
+  const compressed = `${level.charAt(0)}${reduceNumber(timestamp)}:${reduceNumber(processId)}:${reduceNumber(threadId)}${isMainThread ? '!' : ''}:${source}:${message}`;
+  gzBuffer.push(data ? [compressed, data] : compressed);
   if (gzBuffer.length >= GZ_MAX_BUFFER_SIZE) {
     gzFlushBuffer();
   } else if (!gzFlushTimeout) {
@@ -93,22 +49,21 @@ const _log = (attributes: LogAttributes) => log({
   threadId,
   isMainThread,
   ...attributes
-});
+} satisfies InternalLogAttributes & LogAttributes);
 
 const channel = new BroadcastChannel('logger');
 channel.onmessage = (event: any) => {
   if (event.data.terminate) {
-    _log({ message: 'Logger terminating' });
+    _log({ source: 'logger', message: 'Logger terminating' });
     channel.close();
-    db.close();
     gzFlushBuffer();
-    gzipStream.end(() => fileStream.end());
+    gzipStream.end();
   } else if (event.data.isReady) {
     channel.postMessage({ ready: true });
   } else {
-    log(event.data as InternalLogAttributes & LogAttributes);
+    log(event.data);
   }
 }
 
-_log({ message: 'Logger ready' });
+_log({ source: 'logger', message: 'Logger ready' });
 channel.postMessage({ ready: true });
