8982dd927d1df818c3c4ceda2455b1aeca8fb2a4
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-10-25 00:04:29 -0400

    fix: add source

--- main.ts
+++ main.ts
@@ -2,9 +2,12 @@ import { BroadcastChannel, Worker } from 'node:worker_threads';
 import { logger } from './logger.ts';
 import { start } from './threads.ts';
 
-const NUM_APP_WORKERS = 3;
+const NUM_APP_WORKERS = 10;
 
-logger.info({ message: 'Starting applications...' });
+logger.info({ source: 'main', message: 'Starting console...' });
+start('console');
+
+logger.info({ source: 'main', message: 'Starting applications...' });
 
 let appActiveCount = 0;
 const appWorkers: Worker[] = [];
@@ -12,31 +15,31 @@ for (let i = 0; i < NUM_APP_WORKERS; ++i) {
   const id = `App-${i + 1}`;
   const appWorker = start('app', { id });
   appWorker.on('exit', (code) => {
-    logger.warn({ message: `Application worker ${id} exited with code ${code}` });
+    logger.warn({ source: 'main', message: `Application worker ${id} exited with code ${code}` });
     --appActiveCount;
   });
   appWorker.on('error', (err) => {
-    logger.warn({ message: `Application worker ${id} encountered an error:`, error: err });
+    logger.warn({ source: 'main', message: `Application worker ${id} encountered an error:`, error: err });
   });
   appWorkers.push(appWorker);
   ++appActiveCount;
-  logger.info({ message: `Started application worker: ${id}` });
+  logger.info({ source: 'main', message: `Started application worker: ${id}` });
 }
 
-logger.info({ message: `All ${NUM_APP_WORKERS} application workers are running` });
-logger.info({ message: `System is running. Press Ctrl+C to stop` });
+logger.info({ source: 'main', message: `All ${NUM_APP_WORKERS} application workers are running` });
+logger.info({ source: 'main', message: `System is running. Press Ctrl+C to stop` });
 
 const gracefulShutdown = async () => {
-  logger.info({ message: `Received signal to terminate. Shutting down logger worker...` });
+  logger.info({ source: 'main', message: `Received signal to terminate. Shutting down logger worker...` });
   const workersChannel = new BroadcastChannel('apps');
   workersChannel.postMessage({ terminate: true });
   let maxLoops = 20
   while (appActiveCount > 0 && --maxLoops > 0) {
     await new Promise(resolve => setTimeout(resolve, 250));
-    logger.info({ message: `Number of active apps: ${appActiveCount}` });
+    logger.info({ source: 'main', message: `Number of active apps: ${appActiveCount}` });
   }
   if (appActiveCount > 0) {
-    logger.warn({ message: `Forcing shut down...` });
+    logger.warn({ source: 'main', message: `Forcing shut down...` });
     for (const appWorker of appWorkers) {
       appWorker.terminate();
     }
